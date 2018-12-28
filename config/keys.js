8c4dacf3caebef7db48aaa655eb26dcad5dd99a7
OJHA Ayush <ayush.ojha@soprasteria.com>
2018-12-29 00:33:39 +0530

    dev prod configutation added

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
-module.exports = {
-    googleClientID: '594437139180-csci7efsj6ue1g4bvgado5koelpvfh7d.apps.googleusercontent.com',
-    googleClientSecret: 'x3OqFh_ObngBFCgnivdWN4lT',
-    mongoURI: 'mongodb://localhost:27017/colitis',
-    cookieKey: 'svssssvsvsw3r2v'
-};
+if(process.env.NODE_ENV === 'production'){
+    module.export = require('./prod');
+}
+else {
+    module.export = require('./dev');
+}
